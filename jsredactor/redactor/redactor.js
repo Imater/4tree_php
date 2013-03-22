d6bd4ec344bb47ac8ae551d8969cc72db548f5ea
John Wez <eugene.leonar (at) gmail.com>
2013-03-22 11:24:51 +0600

    После отката

--- jsredactor/redactor/redactor.js
+++ jsredactor/redactor/redactor.js
@@ -820,7 +820,6 @@ var RLANG = {
 
 					clearTimeout(my_autosave);
 					note_saved=false;
-					console.info("note_saved=",note_saved);
 					my_autosave = setTimeout( function() { savetext(1); }, 500 );
 					last_editor_click_time = jsNow();
 
