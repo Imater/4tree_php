5baf916e0b5702e5703a9ed66de64eecf2891b75
John Wez <eugene.leonar (at) gmail.com>
2013-06-25 09:47:32 +0600

    fix

--- 4tree.php
+++ 4tree.php
@@ -6,7 +6,7 @@
 <?
 $s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
 $user = json_decode($s, true);
-if($user) print_r($user);
+if(!$user["error"]) print_r($user);
 //$user['network'] - соц. сеть, через которую авторизовался пользователь
 //$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
 //$user['first_name'] - имя пользователя
@@ -77,7 +77,7 @@ $(document).ready(jsDoFirst);
 		<a href="#" id="reg_now">Регистрация (открыта)</a>
 
 <script src="//ulogin.ru/js/ulogin.js"></script>
-<div id="uLogin" data-ulogin="display=panel;fields=first_name,last_name;providers=vkontakte,google,odnoklassniki,mailru,facebook,yandex,twitter;hidden=other;redirect_uri=http%3A%2F%2F4tree.ru%2F4tree.php"></div>
+<div id="uLogin" data-ulogin="display=small;fields=first_name,last_name;providers=vkontakte,google,odnoklassniki,mailru,facebook,yandex,twitter;hidden=other;redirect_uri=http%3A%2F%2F4tree.ru%2F4tree.php"></div>
 		
 		<div id="reg_form" class="myform">
 		  <h2>всего 2 поля:</h2>
