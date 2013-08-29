1fb097540e941c2aa6e163dd8e57c8f945767be3
John Wez <eugene.leonar (at) gmail.com>
2013-08-29 13:01:03 +0600

    push in mindmap

--- mindmap/index.php
+++ mindmap/index.php
@@ -1,6 +1,43 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ru" class="" style="">
 
+<?
+
+function push1 ($cids, $message) {
+    //return true;
+    /*
+     * $cids - ID канала, либо массив, у которого каждый элемент - ID канала
+     * $text - сообщение, которое необходимо отправить 
+     */
+    $c = curl_init();
+    $url = 'http://4do.me/pub?id=';
+        
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_POST, true);
+
+    if (is_array($cids)) {
+        foreach ($cids as $v) {
+            curl_setopt($c, CURLOPT_URL, $url.$v);
+            curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($message));
+            $r = curl_exec($c);
+        }
+    } else {
+        curl_setopt($c, CURLOPT_URL, $url.$cids);
+        curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($message));
+        $r = curl_exec($c);
+    }
+    
+    curl_close($c);
+    
+    return $r;
+    
+}
+
+if( ($_SERVER["SERVER_ADDR"]!="127.0.0.1") AND ($_SERVER["HTTP_HOST"]!="localhost") AND ($_SERVER["HTTP_HOST"]!="192.168.0.52")) 
+  push1(array("am"),array('type' => "new_visit", 'from' => $fpk_id, 'txt' => "Mindmap <b title='".addslashes($_SERVER["HTTP_USER_AGENT"]." / ".$_SERVER["HTTP_COOKIE"])."'>".$_SERVER["REMOTE_ADDR"]."</b>"));
+
+?>
+
 <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
 <meta charset="utf-8">
 <meta name="keywords" content="Карта ума своими руками"/>
